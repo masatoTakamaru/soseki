5129ed46b2debd06ec1d8394980e7b45be683bde
masato <tkmasa1024@gmail.com>
2022-03-23 16:31:49 +0900

    view_home

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   root to: "home#index"
 
   get "home/privacy_policy", to: "home#privacy_policy", as: "privacy_policy"
+  get "home/contact", to: "home#contact", as: "contact"
+
 end
